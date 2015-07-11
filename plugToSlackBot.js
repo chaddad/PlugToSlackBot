69bd241a684590a6cc9a5453b69e2bc660a34dcf
Chris Haddad <chris.haddad@gmail.com>
2015-07-10 22:23:28 -0400

    adding initial TODOs

--- plugToSlackBot.js
+++ plugToSlackBot.js
@@ -1,1 +1,22 @@
-// PlugToSlackBot v0.0
+// PlugToSlackBot v0.01
+
+// TODO: setup initialize loop
+// if (API != 'undefined' || $ != 'undefined')
+
+// TODO: setup initial DJ and Media info
+// API.getDJ(), API.getMedia()
+// DJ.username, Media.author, Media.title
+
+// TODO: set timeout to check back
+// API.getTimeRemaining()
+
+// TODO: after timeout, get new DJ/Media
+// if things have changed, send POST to Slack, otherwise check back in another API.getTimeRemaining()
+
+// TODO: send POST to Slack
+/* $.ajax({
+  type: 'post',
+  url: 'https://hooks.slack.com/services/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',
+  data: '{"text": "This is a test"}',  
+  dataType: 'json'
+}); */
