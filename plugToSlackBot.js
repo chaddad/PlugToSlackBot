02911fe3a7b0b4e556aa2ae131ae1609ce42110e
Chris Haddad <chris.haddad@gmail.com>
2015-07-10 23:49:16 -0400

    added initial setup loop, initial data setup

--- plugToSlackBot.js
+++ plugToSlackBot.js
@@ -1,12 +1,5 @@
 // PlugToSlackBot v0.01
 
-// TODO: setup initialize loop
-// if (API != 'undefined' || $ != 'undefined')
-
-// TODO: setup initial DJ and Media info
-// API.getDJ(), API.getMedia()
-// DJ.username, Media.author, Media.title
-
 // TODO: set timeout to check back
 // API.getTimeRemaining()
 
@@ -20,3 +13,45 @@
   data: '{"text": "This is a test"}',  
   dataType: 'json'
 }); */
+
+var debug = true;
+
+var SlackBot = function () {
+    "use strict";
+    var loaded = false, nextTimeout, currentData, newData, setup;
+    
+    this.pollPlug = function () {
+        return {
+            DJ: API.getDJ(),
+            Media: API.getMedia()
+        };
+    };
+    
+    this.init = function () {
+        this.currentData = this.pollPlug();
+        this.nextTimeout = API.getTimeRemaining();
+    };
+    
+    this.checkInit = function () {
+        if (debug) { console.log("checking slackbot..."); }
+        
+        if (!this.loaded && API !== 'undefined' && $ !== 'undefined') {
+            if (debug) { console.log("loading slackbot"); }
+            
+            this.init();
+            
+            this.loaded = true;
+            
+            clearInterval(this.setup);
+            
+            if (debug) {
+                console.log("slackBot setup");
+                console.info(this.currentData);
+                console.info(this.nextTimeout);
+            }
+        }
+    };
+};
+
+var sb = new SlackBot();
+sb.setup = setInterval(function () { sb.checkInit(); }, 5000);
